294da04110cfe7f53d088f806d29f8fd631ac13a
Vibhav <vibhavyadav13762@gmail.com>
2024-04-09 14:20:13 +0530

    Created user model

---
+++ Backend/Models/user.models.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const UserModel= new mongoose.Schema(
+    {
+        userName:{
+            type:String,
+            required:true,
+            unique:true,
+        },
+       email: {
+        type:String,
+        required:true,
+        unique:true
+        },
+        password:{
+            type:String,
+            required:true,
+        }
+    },{timestamps:true});
+const User=mongoose.model("User",UserModel)
+export default User;
