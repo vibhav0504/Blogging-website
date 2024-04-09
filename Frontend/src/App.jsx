884fe80d007b91f2d6e142b9f23d14a1c4351422
Vibhav <vibhavyadav13762@gmail.com>
2024-04-09 13:47:11 +0530

    Basic Setup created for frontend and backend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+import Dashboard from './Pages/Dashboard'
+import Projects from './Pages/Projects'
+import Header from './Component/Header'
+const App = () => {
+  return (
+  
+    <BrowserRouter>
+    <Header/> 
+      <Routes>
+        <Route  path="/" element={< Home/>}/>
+        <Route  path="/about" element={< About/>}/>
+        <Route  path="/sign-in" element={< Signin/>}/>
+        <Route  path="/sign-up" element={< Signup/>}/>
+        <Route  path="/dashboard" element={< Dashboard/>}/>
+        <Route  path="/project" element={< Projects/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ Frontend/src/Component/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Button, Navbar, TextInput }from "flowbite-react"
+import { Link , useLocation } from 'react-router-dom'
+import { AiOutlineSearch  } from 'react-icons/ai';
+import {FaMoon} from "react-icons/fa"
+
+const Header = () => {
+  const path = useLocation().pathname;
+  return (
+    <div>
+ <Navbar className='border-b-2'>
+    <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+        <span className="rounded-lg text-pink-500 px-1  py-1 bg-gradient-to-r from-indigo-500 to-white">My</span>
+        Blog
+    </Link>
+    <form>
+        <TextInput type='text' placeholder='Search' rightIcon={AiOutlineSearch} className='hidden lg:inline' />
+    </form>
+    <div className='flex gap-4 md:order-2'>
+    <Button className='w-12 h-10 lg:hidden' color="gray" pill><AiOutlineSearch/></Button>
+    <Button className='w-12 h-10 hidden sm:inline' color="gray" pill><FaMoon/></Button>
+    <Link to="/sign-in">
+      <Button gradientDuoTone="purpleToBlue" outline>Sign-In</Button>
+    </Link>
+
+    <Navbar.Toggle/>
+    </div>
+    <Navbar.Collapse>
+      <Navbar.Link active={path==="/"} as={"div"}>
+        <Link to="/">Home</Link>
+      </Navbar.Link>
+      <Navbar.Link active={path==="/about"} as={"div"}>
+        <Link to="/about">About</Link>
+      </Navbar.Link>
+      <Navbar.Link  active={path==="/project"} as={"div"}>
+        <Link to="/project">Projects</Link>
+      </Navbar.Link>
+    </Navbar.Collapse>
+ </Navbar>
+    </div>
+  )
+}
+
+export default Header
