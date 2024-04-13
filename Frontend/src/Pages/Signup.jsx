002fc603c8d074393406500cee10e64bbcf9f87c
Vibhav <vibhavyadav13762@gmail.com>
2024-04-13 12:57:21 +0530

    Revert "Added functionality to signup page and added a proxy server"
    This reverts commit 9a18f9f93ba93a1323013821314fb0d66429dc56.

--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -1,80 +1,9 @@
-import React, { useState } from 'react'
-import {Link ,  useNavigate } from "react-router-dom"
-import { Label, TextInput , Button, Alert, Spinner } from "flowbite-react"
+import React from 'react'
+
 const Signup = () => {
-  const navigate=useNavigate();
-  const[formData,setFormData]=useState({});
-  const[errorMessage,setErrorMessage]=useState(null);
-  const[loading,setLoading]=useState(false)
-  const handleChange=(e)=>{
-    setFormData({...formData,[e.target.id]:e.target.value.trim()})
-    // console.log(formData)
-  }
-const handleSubmit=async(e)=>{
-  e.preventDefault();
-  if(!formData.userName ||!formData.email ||!formData.password ){
-    return setErrorMessage("All Fields are required")
-  }
-  try {
-    setLoading(true);
-    const res=await fetch("/api/signup",{
-    method:"POST",
-    headers:{'Content-Type':"application/json"},
-    body:JSON.stringify(formData)
-    });
-    const data=await res.json();
-    setLoading(false);
-    if(res.ok){
-      navigate('/sign-in')
-    }
-  } catch (error) {
-    setErrorMessage(error.message)
-    setLoading(false)
-  }
-}
-  
   return (
-    <div className='min-h-screen mt-20 '>
-    <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
-{/* For left */}
-    <div className='flex-1'>
-    <Link to="/" className=' font-bold dark:text-white text-4xl'>
-        <span className="rounded-lg text-pink-500 px-1  py-1 bg-gradient-to-r from-indigo-500 to-white">My</span>
-        Blog
-    </Link>
-    <p className='text-sm mt-5'>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem inventore voluptates voluptate unde? Necessitatibus, magni, ea, sequi dignissimos at optio ducimus rem nesciunt ut modi mollitia ratione exercitationem tempore aliquam!
-    </p>
-    </div>
-    {/* For right */}
-    <div className='flex-1  '>
-      <form  className='flex flex-col gap-4 ' onSubmit={handleSubmit}>
-        <div className="">
-          <Label value="Your Username"/>
-<TextInput type="text " placeholder='Username' id='userName' onChange={handleChange}/>
-          <Label value="Your email"/>
-<TextInput type="email " placeholder='name@company.com' id='email' onChange={handleChange}/>
-          <Label value="Your password"/>
-<TextInput type = "password" placeholder='Password' id='password' onChange={handleChange}/>
-        </div>
-       <Button gradientDuoTone='purpleToPink' type="submit" disabled={loading} >{loading? (
-       <>
-       <Spinner size='sm'/>
-       <span>Loading..</span>
-       </>
-       ):"Sign-Up"}</Button> 
-      </form>
-      <div className='flex gap-2 text-sm mt-5'>
-        <span >Have an account?</span>
-       <Link  to="/sign-in" className="text-blue-500">Sign In</Link>
-      </div>
-      {
-        errorMessage && (
-          <Alert className='mt-5' color='failure'>{errorMessage}</Alert>
-        )
-      }
-    </div>
-    </div>
+    <div>
+     Signup 
     </div>
   )
 }
