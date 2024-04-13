6614663450c1a27def6ed7d564121b9f13fb154b
Vibhav <vibhavyadav13762@gmail.com>
2024-04-13 13:01:53 +0530

    Added functionality to sign-up page and added a proxy server

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server:{
+    proxy:{
+      '/api':{
+        target:"http://localhost:7000",
+        secure:false,
+      }
+    }
+      },
 })
