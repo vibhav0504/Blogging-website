dc168f554315f11c23daa50e820f4224768a367e
Vibhav <vibhavyadav13762@gmail.com>
2024-04-23 00:20:40 +0530

    Completed slidebar

---
+++ Frontend/src/Component/Dashprofile.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const Dashprofile = () => {
+  return (
+    <> Dashprofile</>
+  )
+}
+
+export default Dashprofile
--- Frontend/src/Pages/Dashboard.jsx
+++ Frontend/src/Pages/Dashboard.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Dashslidebar from "../Component/Dashslidebar";
+import Dashprofile from "../Component/Dashprofile";
 const Dashboard = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState();
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabfromurl = urlParams.get("tab");
+    if (tabfromurl) {
+      setTab(tabfromurl);
+    }
+  }, [location.search]);
   return (
-    <div>
-      Dashboard
-
+    <div className="min-h-screen flex flex-col md:flex-row ">
+      <div  className=" md:w-56">
+        {/* SlideBar */}
+        <Dashslidebar />
+      </div>
+      {/* Profile */}
+      {tab === "profile" && <Dashprofile />}
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
