2978126bbafa437cf0d7a44aa6a125cdf26f8304
Vibhav <vibhavyadav13762@gmail.com>
2024-04-25 13:52:46 +0530

    create get post api route

--- Backend/Controllers.js/post.controller.js
+++ Backend/Controllers.js/post.controller.js
@@ -1,24 +1,63 @@
-import { errorHandler } from "../utils/error.js"
-import Post from "../Models/post.models.js"
- export const create=async(req,res,next)=>{
-if(!req.user.isAdmin){
-    return next(errorHandler(403,"You are not allowed to create a post"))
-}
-if(!req.body.title||!req.body.content){
-    return next(errorHandler(400,"Plz Provide all details"))
-}
-const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '');  
- const newPost = new Post ({
+import { errorHandler } from "../utils/error.js";
+import Post from "../Models/post.models.js";
+export const create = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to create a post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Plz Provide all details"));
+  }
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "");
+  const newPost = new Post({
     ...req.body,
     slug,
-    userId:req.user.id,
+    userId: req.user.id,
   });
-try {
-    const savedPost=await newPost.save();
+  try {
+    const savedPost = await newPost.save();
     res.status(200).json(savedPost);
-} catch (error) {
-    next(error)
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseFloat(req.query.startIndex) || 0;
+    const limit = parseFloat(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit)
 
-}
+    const totalPosts = await Post.countDocuments();
+    const now=new Date();
+    const oneMonthAgo=new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate()
+    );
+    const lastMonthPosts=await Post.countDocuments({
+        createdAt:{$gte:one}
+    })
+    res.status(200).json({posts,totalPosts,oneMonthAgo})
+  } catch (error) {
+    next(error);
+  }
+};
--- Backend/routes/post.routes.js
+++ Backend/routes/post.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
 import {verifyToken} from "../utils/verifyUser.js"
 const  router=express.Router()
-import { create } from "../Controllers.js/post.controller.js"
+import { create , getPosts } from "../Controllers.js/post.controller.js"
 
-router.post("/create",verifyToken,create)
+router.post("/create",verifyToken,create);
+router.get("/getposts",getPosts)
 
 export default router;
