49daa4d40222b87397bf73081f90ff5eb9c8aab2
Vibhav <vibhavyadav13762@gmail.com>
2024-04-26 11:30:19 +0530

    Added show more functionality

--- Frontend/src/Component/DashPost.jsx
+++ Frontend/src/Component/DashPost.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect ,useState } from 'react'
 import {useSelector} from "react-redux"
-import {Table} from "flowbite-react" 
+import {Button, Table} from "flowbite-react" 
 import {Link} from "react-router-dom"
 const DashPost = () => {
    const[userPost,setUserPost]=useState([])
+   const[showMore,setShowMore]=useState(true);
   const {currentUser}=useSelector(state=>state.user)
-  console.log(userPost)
+  // console.log(userPost)
   useEffect(()=>{
 const fetchPosts=async()=>{
 try {
@@ -16,6 +17,9 @@ try {
 
    if(res.ok){
     setUserPost(data.posts)
+    if(data.length<9){
+      setShowMore(false);
+    }
    }
 } catch (error) {
   console.log(error.message)
@@ -25,9 +29,27 @@ if(currentUser.isAdmin){
   fetchPosts();
 }
   },[currentUser._id])
+ 
+const handleShowMore=async()=>{
+  const startIndex=userPost.length;
+try {
+  const res=await fetch(`/api/getPosts?userId=${currentUser._id}&startIndex=${startIndex}`);
+  console.log(res);
+  const data=await res.json();
+  if(res.ok){
+    setUserPost((prev)=>[...prev,...data.posts])
+    if(data.posts.length<9){
+      setShowMore(false);
+    }
+  }
+} catch (error) {
+  console.log(error.message)
+}
+}
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
    {currentUser.isAdmin && userPost.length>0 ? (
+<>
 <Table hoverable className='shadow-md'>
 <Table.Head>
 <Table.HeadCell>Date Updated </Table.HeadCell>
@@ -66,6 +88,8 @@ return(
 
 })}
 </Table>
+{showMore && (<Button onClick={handleShowMore} className='w-full text-teal-500 self-center bg-white hover:bg-red-300 text-sm py-7'>Show More</Button>)}
+</>
 
    ):(
     <p>You have no posts yet !</p>
