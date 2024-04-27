508d212ede5433d40a76acabde9cec51003b3a25
Vibhav <vibhavyadav13762@gmail.com>
2024-04-27 15:29:42 +0530

    Added delete user functionality

--- Frontend/src/Component/DashUsers.jsx
+++ Frontend/src/Component/DashUsers.jsx
@@ -48,7 +48,21 @@ try {
 }
 
 const handleDeleteUser=async()=>{
-
+   try {
+    const res = await fetch(`/api/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data=await res.json();
+      if(res.ok){
+        setUsers((prev)=>prev.filter((user)=>user._id!==userIdToDelete));
+        setShowModal(false)
+      }
+      else{
+        console.log(data.message)
+      }
+   } catch (error) {
+    
+   }
 }
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
